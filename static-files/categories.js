da1516b575a99b9b791c822d8adccd0c02e54059
yas1nshah <yas1nshah@yahoo.com>
2024-03-24 06:12:19 +0500

    cat files updated

--- static-files/categories.js
+++ static-files/categories.js
@@ -4,7 +4,12 @@ const categories = [
       subcategories: [
         {
           "name": "Low Budget Cars",
-          "link": "/search?priceTo=1000000",
+          "link": "/inventory/search?priceTo=1000000",
+          "image":null
+        },
+        {
+          "name": "High Budget Cars",
+          "link": "/inventory/search?priceFrom=5000000",
           "image":null
         },
         
@@ -15,12 +20,12 @@ const categories = [
       subcategories: [
         {
           "name": "Lahore",
-          "link": "/search?location=Lahore",
+          "link": "/inventory/search?location=Lahore",
           "image":null
         },
         {
           "name": "Islamabad",
-          "link": "/search?location=Islamabad",
+          "link": "/inventory/search?location=Islamabad",
           "image":null
         },
       ]
@@ -30,102 +35,102 @@ const categories = [
       subcategories: [
         {
           "name": "Toyota",
-          "link": "/search?make=Toyota",
+          "link": "/inventory/search?make=Toyota",
           "image": null
         },
         {
           "name": "Suzuki",
-          "link": "/search?make=Suzuki",
+          "link": "/inventory/search?make=Suzuki",
           "image": null
         },
         {
           "name": "Honda",
-          "link": "/search?make=Honda",
+          "link": "/inventory/search?make=Honda",
           "image": null
         },
         {
           "name": "Daihatsu",
-          "link": "/search?make=Daihatsu",
+          "link": "/inventory/search?make=Daihatsu",
           "image": null
         },
         {
           "name": "Kia",
-          "link": "/search?make=Kia",
+          "link": "/inventory/search?make=Kia",
           "image": null
         },
         {
           "name": "Hyundai",
-          "link": "/search?make=Hyundai",
+          "link": "/inventory/search?make=Hyundai",
           "image": null
         },
         {
           "name": "Mitsubishi",
-          "link": "/search?make=Mitsubishi",
+          "link": "/inventory/search?make=Mitsubishi",
           "image": null
         },
         {
           "name": "Changan",
-          "link": "/search?make=Changan",
+          "link": "/inventory/search?make=Changan",
           "image": null
         },
         {
           "name": "Mercedes Benz",
-          "link": "/search?make=Mercedes%20Benz",
+          "link": "/inventory/search?make=Mercedes%20Benz",
           "image": null
         },
         {
           "name": "MG",
-          "link": "/search?make=MG",
+          "link": "/inventory/search?make=MG",
           "image": null
         },
         {
           "name": "Proton",
-          "link": "/search?make=Proton",
+          "link": "/inventory/search?make=Proton",
           "image": null
         },
         {
           "name": "BMW",
-          "link": "/search?make=BMW",
+          "link": "/inventory/search?make=BMW",
           "image": null
         },
         {
           "name": "FAW",
-          "link": "/search?make=FAW",
+          "link": "/inventory/search?make=FAW",
           "image": null
         },
         {
           "name": "DFSK",
-          "link": "/search?make=DFSK",
+          "link": "/inventory/search?make=DFSK",
           "image": null
         },
         {
           "name": "Haval",
-          "link": "/search?make=Haval",
+          "link": "/inventory/search?make=Haval",
           "image": null
         },
         {
           "name": "Peugeot",
-          "link": "/search?make=Peugeot",
+          "link": "/inventory/search?make=Peugeot",
           "image": null
         },
         {
           "name": "Prince",
-          "link": "/search?make=Prince",
+          "link": "/inventory/search?make=Prince",
           "image": null
         },
         {
           "name": "Lexus",
-          "link": "/search?make=Lexus",
+          "link": "/inventory/search?make=Lexus",
           "image": null
         },
         {
           "name": "Chery",
-          "link": "/search?make=Chery",
+          "link": "/inventory/search?make=Chery",
           "image": null
         },
         {
           "name": "Chevrolet",
-          "link": "/search?make=Chevrolet",
+          "link": "/inventory/search?make=Chevrolet",
           "image": null
         }
       ]
@@ -135,137 +140,137 @@ const categories = [
       subcategories: [
         {
           "name": "Corolla",
-          "link": "/search?model=Corolla",
+          "link": "/inventory/search?model=Corolla",
           "image": null
         },
         {
           "name": "Civic",
-          "link": "/search?model=Civic",
+          "link": "/inventory/search?model=Civic",
           "image": null
         },
         {
           "name": "City",
-          "link": "/search?model=City",
+          "link": "/inventory/search?model=City",
           "image": null
         },
         {
           "name": "Alto",
-          "link": "/search?model=Alto",
+          "link": "/inventory/search?model=Alto",
           "image": null
         },
         {
           "name": "Mehran",
-          "link": "/search?model=Mehran",
+          "link": "/inventory/search?model=Mehran",
           "image": null
         },
         {
           "name": "Cultus",
-          "link": "/search?model=Cultus",
+          "link": "/inventory/search?model=Cultus",
           "image": null
         },
         {
           "name": "Wagon R",
-          "link": "/search?model=Wagon%20R",
+          "link": "/inventory/search?model=Wagon%20R",
           "image": null
         },
         {
           "name": "Vitz",
-          "link": "/search?model=Vitz",
+          "link": "/inventory/search?model=Vitz",
           "image": null
         },
         {
           "name": "Swift",
-          "link": "/search?model=Swift",
+          "link": "/inventory/search?model=Swift",
           "image": null
         },
         {
           "name": "Prado",
-          "link": "/search?model=Prado",
+          "link": "/inventory/search?model=Prado",
           "image": null
         },
         {
           "name": "Hilux",
-          "link": "/search?model=Hilux",
+          "link": "/inventory/search?model=Hilux",
           "image": null
         },
         {
           "name": "Mira",
-          "link": "/search?model=Mira",
+          "link": "/inventory/search?model=Mira",
           "image": null
         },
         {
           "name": "Yaris",
-          "link": "/search?model=Yaris",
+          "link": "/inventory/search?model=Yaris",
           "image": null
         },
         {
           "name": "Vezel",
-          "link": "/search?model=Vezel",
+          "link": "/inventory/search?model=Vezel",
           "image": null
         },
         {
           "name": "Sportage",
-          "link": "/search?model=Sportage",
+          "link": "/inventory/search?model=Sportage",
           "image": null
         },
         {
           "name": "Passo",
-          "link": "/search?model=Passo",
+          "link": "/inventory/search?model=Passo",
           "image": null
         },
         {
           "name": "Land Cruiser",
-          "link": "/search?model=Land%20Cruiser",
+          "link": "/inventory/search?model=Land%20Cruiser",
           "image": null
         },
         {
           "name": "Aqua",
-          "link": "/search?model=Aqua",
+          "link": "/inventory/search?model=Aqua",
           "image": null
         },
         {
           "name": "Prius",
-          "link": "/search?model=Prius",
+          "link": "/inventory/search?model=Prius",
           "image": null
         },
         {
           "name": "Bolan",
-          "link": "/search?model=Bolan",
+          "link": "/inventory/search?model=Bolan",
           "image": null
         },
         {
           "name": "Fortuner",
-          "link": "/search?model=Fortuner",
+          "link": "/inventory/search?model=Fortuner",
           "image": null
         },
         {
           "name": "BR-V",
-          "link": "/search?model=BR-V",
+          "link": "/inventory/search?model=BR-V",
           "image": null
         },
         {
           "name": "Coure",
-          "link": "/search?model=Coure",
+          "link": "/inventory/search?model=Coure",
           "image": null
         },
         {
           "name": "Dayz",
-          "link": "/search?model=Dayz",
+          "link": "/inventory/search?model=Dayz",
           "image": null
         },
         {
           "name": "Every",
-          "link": "/search?model=Every",
+          "link": "/inventory/search?model=Every",
           "image": null
         },
         {
           "name": "Tucson",
-          "link": "/search?model=Tucson",
+          "link": "/inventory/search?model=Tucson",
           "image": null
         },
         {
           "name": "C-HR",
-          "link": "/search?model=C-HR",
+          "link": "/inventory/search?model=C-HR",
           "image": null
         }
       ]
@@ -275,62 +280,62 @@ const categories = [
       subcategories: [
         {
           "name": "under 5 Lakh",
-          "link": "/search?priceFrom=0&priceTo=500000",
+          "link": "/inventory/search?priceFrom=0&priceTo=500000",
           "image": null
         },
         {
           "name": "5-10 Lakh",
-          "link": "/search?priceFrom=500000&priceTo=1000000",
+          "link": "/inventory/search?priceFrom=500000&priceTo=1000000",
           "image": null
         },
         {
           "name": "10-20 Lakh",
-          "link": "/search?priceFrom=1000000&priceTo=2000000",
+          "link": "/inventory/search?priceFrom=1000000&priceTo=2000000",
           "image": null
         },
         {
           "name": "20-30 Lakh",
-          "link": "/search?priceFrom=2000000&priceTo=3000000",
+          "link": "/inventory/search?priceFrom=2000000&priceTo=3000000",
           "image": null
         },
         {
           "name": "30-40 Lakh",
-          "link": "/search?priceFrom=3000000&priceTo=4000000",
+          "link": "/inventory/search?priceFrom=3000000&priceTo=4000000",
           "image": null
         },
         {
           "name": "40-50 Lakh",
-          "link": "/search?priceFrom=4000000&priceTo=5000000",
+          "link": "/inventory/search?priceFrom=4000000&priceTo=5000000",
           "image": null
         },
         {
           "name": "50-60 Lakh",
-          "link": "/search?priceFrom=5000000&priceTo=6000000",
+          "link": "/inventory/search?priceFrom=5000000&priceTo=6000000",
           "image": null
         },
         {
           "name": "60-80 Lakh",
-          "link": "/search?priceFrom=6000000&priceTo=8000000",
+          "link": "/inventory/search?priceFrom=6000000&priceTo=8000000",
           "image": null
         },
         {
           "name": "80 Lakh - 1 Crore",
-          "link": "/search?priceFrom=8000000&priceTo=10000000",
+          "link": "/inventory/search?priceFrom=8000000&priceTo=10000000",
           "image": null
         },
         {
           "name": "1-1.5 Crore",
-          "link": "/search?priceFrom=10000000&priceTo=15000000",
+          "link": "/inventory/search?priceFrom=10000000&priceTo=15000000",
           "image": null
         },
         {
           "name": "1.5-2 Crore",
-          "link": "/search?priceFrom=15000000&priceTo=20000000",
+          "link": "/inventory/search?priceFrom=15000000&priceTo=20000000",
           "image": null
         },
         {
           "name": "over 2 Crore",
-          "link": "/search?priceFrom=20000000",
+          "link": "/inventory/search?priceFrom=20000000",
           "image": null
         }
       ]
@@ -340,102 +345,102 @@ const categories = [
       subcategories: [
         {
           name: "Pick Up",
-          link: "/search?bodyType=Pick%20Up",
+          link: "/inventory/search?bodyType=Pick%20Up",
           image: null
         },
         {
           name: "Hatchback",
-          link: "/search?bodyType=Hatchback",
+          link: "/inventory/search?bodyType=Hatchback",
           image: null
         },
         {
           name: "Truck",
-          link: "/search?bodyType=Truck",
+          link: "/inventory/search?bodyType=Truck",
           image: null
         },
         {
           name: "Sedan",
-          link: "/search?bodyType=Sedan",
+          link: "/inventory/search?bodyType=Sedan",
           image: null
         },
         {
           name: "SUV",
-          link: "/search?bodyType=SUV",
+          link: "/inventory/search?bodyType=SUV",
           image: null
         },
         {
           name: "Coupe",
-          link: "/search?bodyType=Coupe",
+          link: "/inventory/search?bodyType=Coupe",
           image: null
         },
         {
           name: "Crossover",
-          link: "/search?bodyType=Crossover",
+          link: "/inventory/search?bodyType=Crossover",
           image: null
         },
         {
           name: "Convertible",
-          link: "/search?bodyType=Convertible",
+          link: "/inventory/search?bodyType=Convertible",
           image: null
         },
         {
           name: "Micro Van",
-          link: "/search?bodyType=Micro%20Van",
+          link: "/inventory/search?bodyType=Micro%20Van",
           image: null
         },
         {
           name: "MPV",
-          link: "/search?bodyType=MPV",
+          link: "/inventory/search?bodyType=MPV",
           image: null
         },
         {
           name: "Mini Van",
-          link: "/search?bodyType=Mini%20Van",
+          link: "/inventory/search?bodyType=Mini%20Van",
           image: null
         },
         {
           name: "Station Wagon",
-          link: "/search?bodyType=Station%20Wagon",
+          link: "/inventory/search?bodyType=Station%20Wagon",
           image: null
         },
         {
           name: "Van",
-          link: "/search?bodyType=Van",
+          link: "/inventory/search?bodyType=Van",
           image: null
         },
         {
           name: "Double Cabin",
-          link: "/search?bodyType=Double%20Cabin",
+          link: "/inventory/search?bodyType=Double%20Cabin",
           image: null
         },
         {
           name: "High Van",
-          link: "/search?bodyType=High%20Van",
+          link: "/inventory/search?bodyType=High%20Van",
           image: null
         },
         {
           name: "High Roof",
-          link: "/search?bodyType=High%20Roof",
+          link: "/inventory/search?bodyType=High%20Roof",
           image: null
         },
         {
           name: "Off-Road Vehicles",
-          link: "/search?bodyType=Off-Road%20Vehicles",
+          link: "/inventory/search?bodyType=Off-Road%20Vehicles",
           image: null
         },
         {
           name: "Single Cabin",
-          link: "/search?bodyType=Single%20Cabin",
+          link: "/inventory/search?bodyType=Single%20Cabin",
           image: null
         },
         {
           name: "Mini Vehicles",
-          link: "/search?bodyType=Mini%20Vehicles",
+          link: "/inventory/search?bodyType=Mini%20Vehicles",
           image: null
         },
         {
           name: "Station Wagom",
-          link: "/search?bodyType=Station%20Wagom",
+          link: "/inventory/search?bodyType=Station%20Wagom",
           image: null
         }
       ]
